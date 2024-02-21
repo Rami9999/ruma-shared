d54b7dbd30fb172799d457cefd33b38a73c3c52e
Rami Abo Ajeeb <rami@zeour.co.uk>
2024-02-21 16:48:53 +0300

    added helpers and github actions

---
+++ src/index.ts
@@ -0,0 +1,114 @@
+export {
+    IAuthPayload,
+    IAuth,
+    IAuthDocument,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    ISignUpPayload,
+    ISignInPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxAuthPayload,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    IAuthResponse,
+    IAuthUser
+} from './interfaces/auth.interface';
+
+export {
+    IBuyerDocument,
+    IReduxBuyer
+} from './interfaces/buyer.interface';
+
+export{
+    IConversationDocument,
+    IMessageDocument,
+    IMessageDetails,
+    IChatBoxProps,
+    IChatSellerProps,
+    IChatBuyerProps,
+    IChatMessageProps,
+} from './interfaces/chat.interface';
+
+export{
+    IEmailLocals
+} from './interfaces/email.interface';
+
+export{
+    ICreateGig,
+    ISellerGig,
+    IGigContext,
+    IGigsProps,
+    IGigCardItems,
+    ISelectedBudget,
+    IGigViewReviewsProps,
+    IGigInfo,
+    IGigTopProps
+} from './interfaces/gig.interface';
+
+export{
+    IOffer,
+    IExtendedDelivery,
+    IDeliveredWork,
+    IOrderEvents,
+    IOrderReview,
+    IOrderMessage,
+    IOrderDocument,
+    IOrderNotifcation,
+
+} from './interfaces/order.interface';
+
+export{
+    IReviewMessageDetails,
+    IRatingTypes,
+    IReviewDocument,
+    IRatingCategoryItem,
+    IRatingCategories,
+
+} from './interfaces/review.interface';
+
+export{
+    ISearchResult,
+    IHitsTotal,
+    IQueryList,
+    IQueryString,
+    IPaginateProps,
+
+} from './interfaces/search.interface';
+
+export{
+    ILanguage,
+    IExperience,
+    IEducation,
+    ICertificate,
+    ISellerDocument,
+    
+} from './interfaces/seller.interface';
+
+export { uploads, videoUpload } from  './cloudinary-upload';
+export {
+    IErrorResponse,
+    IError,
+    CustomError,
+    BadRequestError,
+    NotFoundError,
+    NotAuthorizedError,
+    FileTooLargeError,
+    ServerErrorError,
+    ErrnoException,
+
+} from  './errors-handler';
+
+export { verifyGatewayRequest } from './gateway-middleware';
+
+export {
+    winstonLogger,
+} from './logger';
+
+export {
+    firstLetterUppercase,
+    lowerCase,
+    toUpperCase,
+    isEmail,
+    isDataURL,
+} from './helpers';
